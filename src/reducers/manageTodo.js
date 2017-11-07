8c0d35bf612215eda896fe4c577540493e78bc92
Allen Anderson <dande313@gmail.com>
2017-11-07 02:32:00 +0000

    automatically backed up by learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -4,11 +4,7 @@ export default function manageTodo(state = {
   switch (action.type) {
     case 'ADD_TODO':
       return { todos: state.todos.concat(action.todo.text) };
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 2c407864fee3dddf61f703d1e7b7c88688854569
     default:
       return state;
   }
